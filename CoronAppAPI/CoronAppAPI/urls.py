dca561f870b50d6bf519d54c858a19228588861d
Jhonatan Patrocinio <jhonatan.patrocinio@gmail.com>
2020-03-21 20:20:50 -0500

    fix: routers

--- CoronAppAPI/CoronAppAPI/urls.py
+++ CoronAppAPI/CoronAppAPI/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from rest_framework import routers
 
 from base.api.viewsets import (
@@ -32,4 +32,5 @@ router.register(r'symptomoccurrence', SymptomOccurrenceViewset, basename='Sympto
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls))
 ]
